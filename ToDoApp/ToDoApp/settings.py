8ec47745365379b86ee082813a90ba47f53781c3
Tomas <talvarez2@uc.cl>
2020-01-06 16:08:21 -0300

    ready for heroku

--- ToDoApp/ToDoApp/settings.py
+++ ToDoApp/ToDoApp/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'ToDoApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_app',
+        'USER': 'tomas',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': 5432, 
     }
 }
 
